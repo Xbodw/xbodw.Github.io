3282dd1b5853101a763a9361e4f7b74d288a64db
Xbodw <120997778+Xbodw@users.noreply.github.com>
2023-03-13 09:26:19 +0800

    Create FlutasMain.js

---
+++ Resources/FlutasMain.js
@@ -0,0 +1,15 @@
+"use script";
+//This script by Xbodw.
+//此脚本由Xbodw编写，请勿盗用!
+
+function TapPage(Page) {
+  window.location.pathname=path;
+}
+
+function AddElement(ElementName,GetElement) {
+  var result=document.createElement(ElementName);
+  GetElement.appendChild(result);
+  return result;
+}
+  
+ 
