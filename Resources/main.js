3335ec8189123a759783c4767dbfe68708d4d012
Xbodw <120997778+Xbodw@users.noreply.github.com>
2023-03-31 15:51:59 +0800

    Update main.js

--- Resources/main.js
+++ Resources/main.js
@@ -13,79 +13,310 @@ var AddElement=function(ElementName,GetElement) {
 }
 
 var LoadingPageLevel=function(){
-  var adder= `<div id="preload"><style type="text/css">@keyframes loader {
-    1% {
-        left: -10%;
-        opacity: 1;
-    }
- 
-    60% {
-        left: 110%;
-        opacity: 1;
-    }
- 
-    61% {
-        opacity: 0;
-    }
+  var adder= `<div class="loading">
+  <style><style type="text/css">html,
+bocy,
+div {
+  margin: 0;
+  padding: 0;
 }
  
- 
-#preload {
-    z-index: 1000;
-    position: fixed;
-    width: 100%;
-    height: 100%;
-    background: #1E1E1E;
-    top: 0;
-    left: 0;
-    transition: opacity .3s;
+html,
+body {
+  width: 100%;
+  height: 100%;
 }
  
-    #preload span {
-        display: block;
-        bottom: 45%;
-        height: 10px;
-        width: 10px;
-        position: fixed;
-        background: #0078D7;
-        z-index: 1001;
-        display: inline-block;
-        margin: 0 2px;
-        border-radius: 100%;
-        animation: loader 5s infinite cubic-bezier(0.030, 0.615, 0.995, 0.415);
-        transform: translate(-50%,-50%);
-        opacity: 0;
-    }
+.loading {
+  width: 100%;
+  height: 100%;
+  position:fixed;
+  background-color: black;
+  top: 0;
+            bottom: 0;
+            left: 0;
+            right: 0;
+            margin: auto;
+}
  
-        #preload span:nth-child(1) {
-            animation-delay: 0.5s;
-        }
+.loading i {
+  margin: auto;
+  position: absolute;
+  top: calc(50% - 40px);
+  left: calc(50% - 40px);
+  width: 80px;
+  height: 80px;
+  display: block;
+}
  
-        #preload span:nth-child(2) {
-            animation-delay: 0.4s;
-        }
+.loading span {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  color: black;
+}
  
-        #preload span:nth-child(3) {
-            animation-delay: 0.3s;
-        }
+.loading span:after {
+  content: "";
+  display: block;
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  width: 10px;
+  height: 10px;
+  background: var(--main, #00baff);
+  border-radius: 50%;
+}
  
-        #preload span:nth-child(4) {
-            animation-delay: 0.2s;
-        }
+.loading span:nth-child(1) {
+  animation: i1 5.5s 0.2s infinite;
+}
  
-        #preload span:nth-child(5) {
-            animation-delay: 0.1s;
-        }
+.loading span:nth-child(2) {
+  animation: i2 5.5s 0.4s infinite;
+}
  
-        #preload span:nth-child(6) {
-            animation-delay: 0;
-        }</style>
+.loading span:nth-child(3) {
+  animation: i3 5.5s 0.6s infinite;
+}
+ 
+.loading span:nth-child(4) {
+  animation: i4 5.5s 0.8s infinite;
+}
+ 
+.loading span:nth-child(5) {
+  animation: i5 5.5s 1s infinite;
+}
+ 
+.loading span:nth-child(6) {
+  animation: i6 5.5s 1.2s infinite;
+}
+ 
+@keyframes i1 {
+  0% {
+    opacity: 1;
+    transform: rotate(190deg);
+    animation-timing-function: cubic-bezier(0.29, 0.44, 0.32, 0.74);
+  }
+  7% {
+    opacity: 1;
+    transform: rotate(300deg);
+    animation-timing-function: linear;
+  }
+  30% {
+    opacity: 1;
+    transform: rotate(450deg);
+    animation-timing-function: cubic-bezier(0.53, 0.27, 0.37, 0.81);
+  }
+  39% {
+    opacity: 1;
+    transform: rotate(645deg);
+    animation-timing-function: linear;
+  }
+  63% {
+    opacity: 1;
+    transform: rotate(800deg);
+    animation-timing-function: cubic-bezier(0.5, 0.32, 0.82, 0.54);
+  }
+  68% {
+    opacity: 1;
+    transform: rotate(920deg);
+    animation-timing-function: ease-in;
+  }
+  69% {
+    opacity: 0;
+    transform: rotate(930deg);
+  }
+}
+@keyframes i2 {
+  0% {
+    opacity: 1;
+    transform: rotate(180deg);
+    animation-timing-function: cubic-bezier(0.29, 0.44, 0.32, 0.74);
+  }
+  7% {
+    opacity: 1;
+    transform: rotate(300deg);
+    animation-timing-function: linear;
+  }
+  30% {
+    opacity: 1;
+    transform: rotate(450deg);
+    animation-timing-function: cubic-bezier(0.53, 0.27, 0.37, 0.81);
+  }
+  39% {
+    opacity: 1;
+    transform: rotate(645deg);
+    animation-timing-function: linear;
+  }
+  63% {
+    opacity: 1;
+    transform: rotate(800deg);
+    animation-timing-function: cubic-bezier(0.5, 0.32, 0.82, 0.54);
+  }
+  68% {
+    opacity: 1;
+    transform: rotate(910deg);
+    animation-timing-function: ease-in;
+  }
+  69% {
+    opacity: 0;
+    transform: rotate(920deg);
+  }
+}
+@keyframes i3 {
+  0% {
+    opacity: 1;
+    transform: rotate(170deg);
+    animation-timing-function: cubic-bezier(0.29, 0.44, 0.32, 0.74);
+  }
+  7% {
+    opacity: 1;
+    transform: rotate(300deg);
+    animation-timing-function: linear;
+  }
+  30% {
+    opacity: 1;
+    transform: rotate(450deg);
+    animation-timing-function: cubic-bezier(0.53, 0.27, 0.37, 0.81);
+  }
+  39% {
+    opacity: 1;
+    transform: rotate(645deg);
+    animation-timing-function: linear;
+  }
+  63% {
+    opacity: 1;
+    transform: rotate(800deg);
+    animation-timing-function: cubic-bezier(0.5, 0.32, 0.82, 0.54);
+  }
+  68% {
+    opacity: 1;
+    transform: rotate(900deg);
+    animation-timing-function: ease-in;
+  }
+  69% {
+    opacity: 0;
+    transform: rotate(910deg);
+  }
+}
+@keyframes i4 {
+  0% {
+    opacity: 1;
+    transform: rotate(160deg);
+    animation-timing-function: cubic-bezier(0.29, 0.44, 0.32, 0.74);
+  }
+  7% {
+    opacity: 1;
+    transform: rotate(300deg);
+    animation-timing-function: linear;
+  }
+  30% {
+    opacity: 1;
+    transform: rotate(450deg);
+    animation-timing-function: cubic-bezier(0.53, 0.27, 0.37, 0.81);
+  }
+  39% {
+    opacity: 1;
+    transform: rotate(645deg);
+    animation-timing-function: linear;
+  }
+  63% {
+    opacity: 1;
+    transform: rotate(800deg);
+    animation-timing-function: cubic-bezier(0.5, 0.32, 0.82, 0.54);
+  }
+  68% {
+    opacity: 1;
+    transform: rotate(890deg);
+    animation-timing-function: ease-in;
+  }
+  69% {
+    opacity: 0;
+    transform: rotate(900deg);
+  }
+}
+@keyframes i5 {
+  0% {
+    opacity: 1;
+    transform: rotate(150deg);
+    animation-timing-function: cubic-bezier(0.29, 0.44, 0.32, 0.74);
+  }
+  7% {
+    opacity: 1;
+    transform: rotate(300deg);
+    animation-timing-function: linear;
+  }
+  30% {
+    opacity: 1;
+    transform: rotate(450deg);
+    animation-timing-function: cubic-bezier(0.53, 0.27, 0.37, 0.81);
+  }
+  39% {
+    opacity: 1;
+    transform: rotate(645deg);
+    animation-timing-function: linear;
+  }
+  63% {
+    opacity: 1;
+    transform: rotate(800deg);
+    animation-timing-function: cubic-bezier(0.5, 0.32, 0.82, 0.54);
+  }
+  68% {
+    opacity: 1;
+    transform: rotate(880deg);
+    animation-timing-function: ease-in;
+  }
+  69% {
+    opacity: 0;
+    transform: rotate(880deg);
+  }
+}
+@keyframes i6 {
+  0% {
+    opacity: 1;
+    transform: rotate(140deg);
+    animation-timing-function: cubic-bezier(0.29, 0.44, 0.32, 0.74);
+  }
+  7% {
+    opacity: 1;
+    transform: rotate(300deg);
+    animation-timing-function: linear;
+  }
+  30% {
+    opacity: 1;
+    transform: rotate(450deg);
+    animation-timing-function: cubic-bezier(0.53, 0.27, 0.37, 0.81);
+  }
+  39% {
+    opacity: 1;
+    transform: rotate(645deg);
+    animation-timing-function: linear;
+  }
+  63% {
+    opacity: 1;
+    transform: rotate(800deg);
+    animation-timing-function: cubic-bezier(0.5, 0.32, 0.82, 0.54);
+  }
+  68% {
+    opacity: 1;
+    transform: rotate(870deg);
+    animation-timing-function: ease-in;
+  }
+  69% {
+    opacity: 0;
+    transform: rotate(880deg);
+  }
+}</style>
+    <i>
         <span></span>
         <span></span>
         <span></span>
         <span></span>
         <span></span>
         <span></span>
+    </i>
 </div>`;
     var add = document.createElement('div');
     add.innerHTML = adder;
@@ -93,7 +324,7 @@ var LoadingPageLevel=function(){
 }
 
 var DeleteLoadingPageLevel= function() {
-  document.querySelector('div[id="preload"]').remove();
+  document.querySelector('div[id="loading"]').remove();
 }
 
 var checkm=function() {
