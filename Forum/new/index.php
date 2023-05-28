8043c308fe1763e12638c4cead6c3c725a18abf5
Xbodw <120997778+Xbodw@users.noreply.github.com>
2023-05-28 10:45:36 +0800

    Update and rename index.html to index.php

---
+++ Forum/new/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>在论坛发布文章 - Flutas</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="./Resources/Feature.css">
+    <link href="./Resources/Flutas.ico" rel="icon">
+    <meta name="xbodw.github.io" content="Flutas">
+</head>
+<body>
+    <form method="post">
+        <label for="content">请输入内容：</label><br>
+        <textarea name="content" id="content"rows="10" cols="45"></textarea><br>
+        <label for="email">请输入邮箱：</label>
+        <input type="email" name="email" id="email" required><br>
+        <button><a href="index.html">返回</a></button> <input type="submit" name="submit" value="上传">
+        <hr/>
+    </form>
+    </body>
+</html>
